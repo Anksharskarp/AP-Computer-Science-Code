3afb367706a19562bdf6c15b4dcd2c675a7ad748
William Zhang <58315985+Anksharskarp@users.noreply.github.com>
2020-12-08 18:04:35 -0500

    Add files via upload

---
+++ Java-Class-22/Homework22Part1.java
@@ -0,0 +1,18 @@
+//Java program that removes duplicates char in String
+public class Homework22Part1 {
+    public static void main(String[] args) {
+        String s = "hellollo";
+        unrepeated(s);
+    }
+
+    public static void unrepeated(String string) {
+        String result = "";
+
+        for (int i = 0; i < string.length(); i++) {
+            if (!result.contains(string.substring(i, i + 1))) {
+                result = result + string.substring(i, i + 1);
+            }
+        }
+        System.out.println(result);
+    }
+}
