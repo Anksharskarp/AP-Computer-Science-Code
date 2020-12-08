084384e5ca2897e4908694fd5a5c5d0169978a68
William Zhang <58315985+Anksharskarp@users.noreply.github.com>
2020-12-08 18:02:59 -0500

    Add files via upload

---
+++ Java-Class-11/Class11Part5.java
@@ -0,0 +1,8 @@
+public class Class11Part5 {
+    public static void main(String[] args) {
+        int[] a1 = {1, 2};
+        int[] b1 = {3, 4};
+        a1 = b1;
+        System.out.print(a1[1]);
+    }
+}
