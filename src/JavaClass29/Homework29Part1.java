e7440d26fd468349781ed357ca24afebd4a09872
Anksharskarp <58315985+Anksharskarp@users.noreply.github.com>
2021-01-27 10:24:20 -0500

    End of Class #29

---
+++ src/JavaClass29/Homework29Part1.java
@@ -0,0 +1,11 @@
+package JavaClass29;
+
+public class Homework29Part1 {
+
+    public Homework29Part1() {
+
+    }
+    public static void main(String[] args) {
+        new åHomework29Part1();
+    }
+}
