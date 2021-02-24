f456403a86e0817fdd9b4ad299df117426ddc27d
Anksharskarp <58315985+Anksharskarp@users.noreply.github.com>
2021-02-23 19:05:02 -0500

    Nearly towards the end of class, started on the money change calculator application.

--- src/JavaClass32/Class32Part1.java
+++ src/JavaClass32/Class32Part1.java
@@ -1,7 +1,56 @@
 package JavaClass32;
 
-public class Class32Part1 {
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+import java.awt.*;
+
+public class Class32Part1 extends JFrame {
+    private JTextField amountField = new JTextField(12);
+    private Document amountText = amountField.getDocument();
+    private JTextArea report = new JTextArea(8, 40);
+
+    public Class32Part1() {
+        setSize(500, 500);
+        JPanel topPanel = new JPanel();
+        topPanel.add(new JLabel("Amount:"));
+        topPanel.add(amountField);
+
+        getContentPane().add(topPanel, BorderLayout.NORTH);
+        getContentPane().add(new JScrollPane(report));
+
+        setBackground(Color.LIGHT_GRAY);
+        setTitle("Changer");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        amountText.addDocumentListener(new DocumentListener() {
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                showResult();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                showResult();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                showResult();
+            }
+
+        });
+    }
+
+    public void showResult() {
+        report.setText("Result will show here.");
+    }
+
     public static void main(String[] args) {
-        
+        new Class32Part1();
+
     }
 }
